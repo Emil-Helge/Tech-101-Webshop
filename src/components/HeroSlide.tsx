fd390cfa79c999b68f7348144c12ccdeb3aa3faa
Hampus Isebring <hampus.isebring@gmail.com>
2023-03-11 19:43:06 +0100

    work in progress

--- src/components/HeroSlide.tsx
+++ src/components/HeroSlide.tsx
@@ -1,4 +1,4 @@
-import { Box } from "@mantine/core";
+import { Box, MediaQuery } from "@mantine/core";
 import { Autoplay, Navigation, Pagination } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/swiper-bundle.css";
@@ -8,30 +8,32 @@ import HeroSlideItem from "./HeroSlideItem";
 
 function HeroSlide() {
   return (
-    <Box
-      sx={{
-        paddingTop: "1rem",
-        paddingBottom: "1rem",
-        width: "80%",
-        margin: "0 auto",
-      }}
-    >
-      <Swiper
-        spaceBetween={0}
-        slidesPerView={1}
-        grabCursor={true}
-        modules={[Autoplay, Navigation, Pagination]}
-        autoplay={{ delay: 4000, disableOnInteraction: false }}
-        navigation
-        pagination={{ clickable: true }}
+    <MediaQuery query="(max-width: 730px)" styles={{ flexDirection: "column" }}>
+      <Box
+        sx={{
+          paddingTop: "1rem",
+          paddingBottom: "1rem",
+          width: "100%",
+          margin: "0 auto",
+        }}
       >
-        {mockedProducts.slice(0, 4).map((product) => (
-          <SwiperSlide style={{ height: "25rem" }} key={product.id}>
-            <HeroSlideItem imageSrc={product.image} />
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </Box>
+        <Swiper
+          spaceBetween={0}
+          slidesPerView={1}
+          grabCursor={true}
+          modules={[Autoplay, Navigation, Pagination]}
+          autoplay={{ delay: 4000, disableOnInteraction: false }}
+          navigation
+          pagination={{ clickable: true }}
+        >
+          {mockedProducts.slice(0, 4).map((product) => (
+            <SwiperSlide style={{ height: "25rem" }} key={product.id}>
+              <HeroSlideItem imageSrc={product.image} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </Box>
+    </MediaQuery>
   );
 }
 
--- src/components/HeroSlideItem.tsx
+++ src/components/HeroSlideItem.tsx
@@ -1,4 +1,4 @@
-import { Box, Button } from "@mantine/core";
+import { Box, Button, MediaQuery, Title } from "@mantine/core";
 
 interface HeroSlideItemProps {
   imageSrc?: string;
@@ -6,49 +6,62 @@ interface HeroSlideItemProps {
 
 function HeroSlideItem({ imageSrc }: HeroSlideItemProps) {
   return (
-    <Box sx={{ display: "flex", height: "100%", width: "100%" }}>
-      <Box
-        sx={{
-          width: "50%",
-          height: "100%",
-          backgroundColor: "#283BA2",
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-          color: "white",
-          textAlign: "center",
-          padding: "2rem",
-        }}
-      >
-        <h1>
-          Shop Easter & New <br /> Weekend Deals
-        </h1>
-        <h2>We got you covered on all the latest releases in tech.</h2>
-        <Button
-          sx={{
-            background: "#D9D9D9",
-            color: "#283BA2",
-            fontWeight: "bold",
-            marginTop: "1rem",
-            height: "2.4rem",
-            width: "8rem",
-            borderRadius: ".5rem",
-            borderStyle: "none",
-            cursor: "pointer",
-          }}
+    <MediaQuery
+      query="(max-width: 730px)"
+      styles={{ flexDiorection: "column" }}
+    >
+      <Box sx={{ display: "flex", height: "100%", width: "100%" }}>
+        <MediaQuery query="(max-width: 730px)" styles={{ width: "100%" }}>
+          <Box
+            sx={{
+              width: "50%",
+              height: "100%",
+              backgroundColor: "#283BA2",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+              color: "white",
+              padding: "2rem",
+            }}
+          >
+            <Title>
+              Shop Easter & <br /> New Weekend <br /> Deals
+            </Title>
+            <Title order={4}>
+              We got you covered on all the latest releases in tech.
+            </Title>
+            <Button
+              sx={{
+                background: "#D9D9D9",
+                color: "#283BA2",
+                fontWeight: "bold",
+                marginTop: "1rem",
+                height: "2.4rem",
+                width: "8rem",
+                borderRadius: ".5rem",
+                borderStyle: "none",
+                cursor: "pointer",
+              }}
+            >
+              Go to Deals
+            </Button>
+          </Box>
+        </MediaQuery>
+        <MediaQuery
+          query="(max-width: 730px)"
+          styles={{ height: "100%", width: "50%" }}
         >
-          Go to Deals
-        </Button>
+          <Box sx={{ width: "50%", height: "100%" }}>
+            <img
+              style={{ width: "100%", height: "100%", objectFit: "cover" }}
+              src={imageSrc}
+              alt="product-image"
+            />
+          </Box>
+        </MediaQuery>
       </Box>
-      <Box sx={{ width: "50%", height: "100%" }}>
-        <img
-          style={{ width: "100%", height: "100%", objectFit: "cover" }}
-          src={imageSrc}
-          alt="product-image"
-        />
-      </Box>
-    </Box>
+    </MediaQuery>
   );
 }
 
