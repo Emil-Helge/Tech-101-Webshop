d52db7fbd117affc7edf79a9404ad32e90e674b8
Hampus Isebring <hampus.isebring@gmail.com>
2023-03-13 14:31:17 +0100

    Added icons and new content to homepage.

--- src/components/HeroSlide.tsx
+++ src/components/HeroSlide.tsx
@@ -1,4 +1,4 @@
-import { Box, MediaQuery } from "@mantine/core";
+import { Box } from "@mantine/core";
 import { Autoplay, Navigation, Pagination } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/swiper-bundle.css";
@@ -8,32 +8,30 @@ import HeroSlideItem from "./HeroSlideItem";
 
 function HeroSlide() {
   return (
-    <MediaQuery query="(max-width: 730px)" styles={{ flexDirection: "column" }}>
-      <Box
-        sx={{
-          paddingTop: "1rem",
-          paddingBottom: "1rem",
-          width: "100%",
-          margin: "0 auto",
-        }}
+    <Box
+      sx={{
+        paddingTop: "1rem",
+        paddingBottom: "1rem",
+        width: "100%",
+        margin: "0 auto",
+      }}
+    >
+      <Swiper
+        spaceBetween={0}
+        slidesPerView={1}
+        grabCursor={true}
+        modules={[Autoplay, Navigation, Pagination]}
+        autoplay={{ delay: 4000, disableOnInteraction: false }}
+        navigation
+        pagination={{ clickable: true }}
       >
-        <Swiper
-          spaceBetween={0}
-          slidesPerView={1}
-          grabCursor={true}
-          modules={[Autoplay, Navigation, Pagination]}
-          autoplay={{ delay: 4000, disableOnInteraction: false }}
-          navigation
-          pagination={{ clickable: true }}
-        >
-          {mockedProducts.slice(0, 4).map((product) => (
-            <SwiperSlide style={{ height: "25rem" }} key={product.id}>
-              <HeroSlideItem imageSrc={product.image} />
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </Box>
-    </MediaQuery>
+        {mockedProducts.slice(0, 4).map((product) => (
+          <SwiperSlide style={{ height: "22rem" }} key={product.id}>
+            <HeroSlideItem imageSrc={product.image} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </Box>
   );
 }
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -42,6 +42,7 @@ const useStyles = createStyles((theme) => ({
     justifyContent: "space-between",
     alignItems: "center",
     height: "100%",
+    width: "100%",
   },
 
   links: {
@@ -147,49 +148,36 @@ export function HeaderResponsive({ links }: HeaderResponsiveProps) {
       ref={headerRef}
       className={classes.root}
     >
-      <Container className={classes.header}>
+      <Container sx={{ maxWidth: "none" }} className={classes.header}>
         <Group spacing={5} className={classes.links}>
           {items}
         </Group>
 
-        <Button variant="subtle">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="icon icon-tabler icon-tabler-shopping-cart"
-            width="30"
-            height="30"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            fill="none"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-            <path d="M6 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"></path>
-            <path d="M17 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"></path>
-            <path d="M17 17h-11v-14h-2"></path>
-            <path d="M6 5l14 1l-1 7h-13"></path>
-          </svg>
-          <div
-            style={{
-              borderRadius: "10rem",
-              background: "navy",
-              color: "white",
-              width: "1.2rem",
-              height: "1.2rem",
-              position: "absolute",
-              bottom: 0,
-              right: 0,
-              display: "flex",
-              transform: "translate(-30%, -75%)",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            3
-          </div>
-        </Button>
+        <Group spacing={1}>
+          <img src="./assets/admin-icon.svg" alt="admin icon" />
+
+          <Button variant="subtle">
+            <img src="./assets/shopping-cart.svg" alt="shopping cart icon" />
+            <div
+              style={{
+                borderRadius: "10rem",
+                background: "navy",
+                color: "white",
+                width: "1.2rem",
+                height: "1.2rem",
+                position: "absolute",
+                bottom: 0,
+                right: 0,
+                display: "flex",
+                transform: "translate(-30%, -95%)",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              3
+            </div>
+          </Button>
+        </Group>
 
         <Burger
           opened={opened}
