861c4b4ed2c169191491a9acdc36489f4d7dc18d
Sebastianjohansson123 <johansson.sebastian92@hotmail.com>
2023-03-13 15:22:52 +0100

    changed some things back after consulting with lé B0iZ

--- src/components/CartProduct.tsx
+++ src/components/CartProduct.tsx
@@ -1,5 +1,4 @@
 import { Button, Card, Group, Image, Text } from '@mantine/core';
-import { useEffect, useState } from 'react';
 import { useShoppingCart } from '../context/ShoppingCartContext';
 import { mockedProducts } from '../data';
 
@@ -13,111 +12,51 @@ function CartProduct({ id, quantity }: CartProductProps) {
     useShoppingCart();
   const product = mockedProducts.find((i) => i.id === id);
   if (product == null) return null;
-  const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth < 700);
 
-  useEffect(() => {
-    const handleResize = () => {
-      setIsSmallScreen(window.innerWidth < 700);
-    };
-
-    window.addEventListener('resize', handleResize);
-
-    // cleanup function
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
-  if (!isSmallScreen) {
-    return (
-      <Card shadow="sm" mt="sm" padding="sm" radius="sm">
-        <Card.Section>
-          <Image src={product.image} height={120} fit="cover" />
-        </Card.Section>
-        <Group position="center" mt="sm" mb="sm">
-          <Text weight={500} size={20} transform="uppercase">
-            {product.title}
-          </Text>
-        </Group>
-        <Group position="center" mt="xs" mb="xs">
-          <Button
-            variant="light"
-            mt="sm"
-            radius="sm"
-            onClick={() => decreaseCartQuantity(product.id)}
-          >
-            -
-          </Button>
-          <Text mt="sm" weight={300} size={15}>
-            x{quantity}
-          </Text>
-          <Button
-            variant="light"
-            mt="sm"
-            radius="md"
-            onClick={() => increaseCartQuantity(product.id)}
-          >
-            +
-          </Button>
-          <Button
-            variant="light"
-            mt="sm"
-            radius="md"
-            onClick={() => removeFromCart(product.id)}
-          >
-            Remove
-          </Button>
-          <Text mt="sm" weight={500} size={15}>
-            x{product.price * quantity}€
-          </Text>
-        </Group>
-      </Card>
-    );
-  } else {
-    return (
-      <Card shadow="sm" mt="sm" padding="sm" radius="sm">
-        <Card.Section>
-          <Image src={product.image} height={120} fit="cover" />
-        </Card.Section>
-        <Group position="center" mt="sm" mb="sm">
-          <Text weight={500} size={20} transform="uppercase">
-            {product.title}
-          </Text>
-        </Group>
-        <Group position="center" mt="xs" mb="xs">
-          <Button
-            variant="light"
-            mt="sm"
-            radius="sm"
-            onClick={() => decreaseCartQuantity(product.id)}
-          >
-            -
-          </Button>
-          <Text mt="sm" weight={300} size={15}>
-            x{quantity}
-          </Text>
-          <Button
-            variant="light"
-            mt="sm"
-            radius="md"
-            onClick={() => increaseCartQuantity(product.id)}
-          >
-            +
-          </Button>
-          <Button
-            variant="light"
-            mt="sm"
-            radius="md"
-            onClick={() => removeFromCart(product.id)}
-          >
-            Remove
-          </Button>
-          <Text mt="sm" weight={500} size={15}>
-            x{product.price * quantity}€
-          </Text>
-        </Group>
-      </Card>
-    );
-  }
+  return (
+    <Card shadow="sm" mt="sm" padding="sm" radius="sm">
+      <Card.Section>
+        <Image src={product.image} height={120} fit="cover" />
+      </Card.Section>
+      <Group position="center" mt="sm" mb="sm">
+        <Text weight={500} size={20} transform="uppercase">
+          {product.title}
+        </Text>
+      </Group>
+      <Group position="center" mt="xs" mb="xs">
+        <Button
+          variant="light"
+          mt="sm"
+          radius="sm"
+          onClick={() => decreaseCartQuantity(product.id)}
+        >
+          -
+        </Button>
+        <Text mt="sm" weight={300} size={15}>
+          x{quantity}
+        </Text>
+        <Button
+          variant="light"
+          mt="sm"
+          radius="md"
+          onClick={() => increaseCartQuantity(product.id)}
+        >
+          +
+        </Button>
+        <Button
+          variant="light"
+          mt="sm"
+          radius="md"
+          onClick={() => removeFromCart(product.id)}
+        >
+          Remove
+        </Button>
+        <Text mt="sm" weight={500} size={15}>
+          x{product.price * quantity}€
+        </Text>
+      </Group>
+    </Card>
+  );
 }
 
 export default CartProduct;
