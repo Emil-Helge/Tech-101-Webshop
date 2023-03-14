ace0f24fd2a7f0be59e1ecfb688a69a5b3dd6942
Sebastianjohansson123 <johansson.sebastian92@hotmail.com>
2023-03-14 22:51:48 +0100

    created a adminProduct component

---
+++ src/components/AdminProductCard.tsx
@@ -0,0 +1,56 @@
+import { Button, Card, Group, Image, Text } from '@mantine/core';
+import { Product } from '../../data/index';
+import { useShoppingCart } from '../contexts/ShoppingCartContext';
+
+interface Props {
+  product: Product;
+  isAdmin?: boolean;
+  onDelete?: () => void;
+}
+
+function AdminProductCard({ product, onDelete, isAdmin }: Props) {
+  const {
+    getProductQuantity,
+    increaseCartQuantity,
+    decreaseCartQuantity, // Not in use yet
+    removeFromCart, // Not in use yet
+  } = useShoppingCart();
+  const quantity = getProductQuantity(product.id);
+  return (
+    <>
+      <Card shadow="xl" padding="md" radius="lg" withBorder>
+        <Card.Section>
+          <Image src={product.image} height={230} fit="cover" />
+        </Card.Section>
+        <Group position="center" mt="xl" mb="xl">
+          <Text weight={500} size={29} transform="uppercase">
+            {product.title}
+          </Text>
+        </Group>
+        <Text size="sm" color="dimmed" align="center">
+          {product.description}
+        </Text>
+        <Text weight={500} size="lg" color="dark" align="center">
+          {product.price}€
+        </Text>
+        <Group position="center" mt="md" mb="xs">
+          <Button variant="danger" mt="md" radius="md" onClick={onDelete}>
+            Delete Product
+          </Button>
+          <Button
+            variant="light"
+            mt="md"
+            radius="md"
+            onClick={() => increaseCartQuantity(product.id)}
+          >
+            Add to cart
+          </Button>
+        </Group>
+      </Card>
+    </>
+  );
+}
+
+export default AdminProductCard;
+
+//-------------------------------------------
