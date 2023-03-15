cf62313e2de48ad409431a533bb807d8525ce19d
Sebastianjohansson123 <johansson.sebastian92@hotmail.com>
2023-03-15 21:48:01 +0100

    added functionality to "cart is empty" but not added the right content or styling yet

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -64,85 +64,81 @@ function Cart() {
       </Card>
     );
   }
-
-  return (
-    <Container size="md">
-      <Grid justify="center" align="flex-start">
-        <Grid.Col span={10} md={8} sm="auto">
-          {cartProducts.map((product) => (
-            <CartProduct key={product.id} {...product} />
-          ))}
-          <CheckoutForm />
-        </Grid.Col>
-        <Grid.Col span={12} sm="auto">
-          <Card
-            sx={{
-              display: 'flex',
-              gap: '1rem',
-              flexDirection: 'column',
-              justifyItems: 'center',
-              alignItems: 'center',
-              marginTop: '0.7rem',
-              border: '0.15rem solid',
-              borderColor: theme.colors.blue[0],
-            }}
-          >
-            <Text weight={600} size={25}>
-              Summary:
-            </Text>
-            <Text weight={500} size={18}>
-              {cartProducts.map((cartproduct) => {
-                const product = mockedProducts.find(
-                  (i) => i.id === cartproduct.id
-                );
-                return (
-                  <Box
-                    key={cartproduct.id}
-                    sx={{
-                      display: 'flex',
-                      flexDirection: 'row',
-                      gap: '1rem',
-                      justifyContent: 'space-between',
-                    }}
-                  >
-                    <Text>{product?.title}</Text>
-                    <Text weight={400}>
-                      {cartproduct.quantity}x {product?.price}€
-                    </Text>
-                  </Box>
-                );
-              })}
-            </Text>
-            <Text weight={500} size={29}>
-              total:{' '}
-              {cartProducts.reduce((total, cartProduct) => {
-                const product = mockedProducts.find(
-                  (i) => i.id === cartProduct.id
-                );
-                return total + (product?.price || 0) * cartProduct.quantity;
-              }, 0)}
-              €
-            </Text>
-            {showLastOrder ? (
-              <>
-                <Button> Checkout </Button>
-                <button onClick={() => setShowLastOrder(!showLastOrder)}>
-                  hide
-                </button>
-              </>
-            ) : (
-              <>
-                <button onClick={() => setShowLastOrder(!showLastOrder)}>
-                  show
-                </button>
-              </>
-            )}
-          </Card>
-          {renderLastOrder()}
-        </Grid.Col>
-      </Grid>
-    </Container>
-  );
+  if (!showLastOrder) {
+    return (
+      <Container size="md">
+        <Grid justify="center" align="flex-start">
+          <Grid.Col span={10} md={8} sm="auto">
+            {cartProducts.map((product) => (
+              <CartProduct key={product.id} {...product} />
+            ))}
+            <CheckoutForm />
+          </Grid.Col>
+          <Grid.Col span={12} sm="auto">
+            <Card
+              sx={{
+                display: 'flex',
+                gap: '1rem',
+                flexDirection: 'column',
+                justifyItems: 'center',
+                alignItems: 'center',
+                marginTop: '0.7rem',
+                border: '0.15rem solid',
+                borderColor: theme.colors.blue[0],
+              }}
+            >
+              <Text weight={600} size={25}>
+                Summary:
+              </Text>
+              <Text weight={500} size={18}>
+                {cartProducts.map((cartproduct) => {
+                  const product = mockedProducts.find(
+                    (i) => i.id === cartproduct.id
+                  );
+                  return (
+                    <Box
+                      key={cartproduct.id}
+                      sx={{
+                        display: 'flex',
+                        flexDirection: 'row',
+                        gap: '1rem',
+                        justifyContent: 'space-between',
+                      }}
+                    >
+                      <Text>{product?.title}</Text>
+                      <Text weight={400}>
+                        {cartproduct.quantity}x {product?.price}€
+                      </Text>
+                    </Box>
+                  );
+                })}
+              </Text>
+              <Text weight={500} size={29}>
+                total:{' '}
+                {cartProducts.reduce((total, cartProduct) => {
+                  const product = mockedProducts.find(
+                    (i) => i.id === cartProduct.id
+                  );
+                  return total + (product?.price || 0) * cartProduct.quantity;
+                }, 0)}
+                €
+              </Text>
+              <Button> Checkout </Button>
+              <button onClick={() => setShowLastOrder(!showLastOrder)}>
+                hide
+              </button>
+              <button onClick={() => setShowLastOrder(!showLastOrder)}>
+                show
+              </button>
+            </Card>
+            {renderLastOrder()}
+          </Grid.Col>
+        </Grid>
+      </Container>
+    );
+  } else {
+    return <h1>NU SKÄMDE DU UT DIG!!</h1>;
+  }
 }
 
 export default Cart;
