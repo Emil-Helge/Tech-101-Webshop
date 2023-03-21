4bf58015e4936039d23cdafae3efb75dca2df7e9
Gabriel Lugo Méndez <gabriel.lugo.mendez@gmail.com>
2023-03-21 10:29:01 +0100

    add form to EditProduct

--- src/pages/EditProduct.tsx
+++ src/pages/EditProduct.tsx
@@ -1,10 +1,21 @@
-import { Group, Title } from '@mantine/core';
+import { Container, Group, Title } from '@mantine/core';
+import { useContext } from 'react';
+import ProductForm from '../components/ProductForm';
+import { ProductContext } from '../contexts/ProductContext';
 
 function EditProduct() {
+  const { addProduct } = useContext(ProductContext);
+
   return (
-    <Group>
-      <Title>Edit</Title>
-    </Group>
+    <Container>
+      <Group position="center" mb="xl">
+        <Title>Edit Product</Title>
+      </Group>
+      <ProductForm
+        onSubmit={(product) => console.log(product)}
+        addProduct={addProduct}
+      />
+    </Container>
   );
 }
 
