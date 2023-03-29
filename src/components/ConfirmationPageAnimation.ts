581dc9daa4e2ed40890595db262adaff57f1bf84
Emil Helgesson <helgesson.emil.94@gmail.com>
2023-03-29 20:00:44 +0200

    changed shape to a smiley and adjusted values

--- src/components/ConfirmationPageAnimation.ts
+++ src/components/ConfirmationPageAnimation.ts
@@ -3,6 +3,9 @@ import { useMantineTheme } from '@mantine/core';
 function initBackgroundAnimation() {
   const theme = useMantineTheme();
   const canvas = document.createElement('canvas');
+  const customSvgPath =
+    'M17 3.34a10 10 0 1 1 -14.995 8.984l-.005 -.324l.005 -.324a10 10 0 0 1 14.995 -8.336zm-2 9.66h-6a1 1 0 0 0 -1 1v.05a3.975 3.975 0 0 0 3.777 3.97l.227 .005a4.026 4.026 0 0 0 3.99 -3.79l.006 -.206a1 1 0 0 0 -1 -1.029zm-5.99 -5l-.127 .007a1 1 0 0 0 .117 1.993l.127 -.007a1 1 0 0 0 -.117 -1.993zm6 0l-.127 .007a1 1 0 0 0 .117 1.993l.127 -.007a1 1 0 0 0 -.117 -1.993z';
+
   canvas.style.position = 'fixed';
   canvas.style.zIndex = '-1';
   canvas.style.top = '0';
@@ -39,39 +42,18 @@ function initBackgroundAnimation() {
       this.x += this.speedX;
       this.y += this.speedY;
 
-      if (this.size > 0.2) this.size -= 0.05;
+      if (this.size > 6) this.size -= 0.045;
     }
 
-    draw() {
-      ctx.fillStyle = theme.colors.blue[2];
-      ctx.strokeStyle = theme.colors.blue[2];
-      ctx.lineWidth = 2;
-      ctx.beginPath();
-      const spikes = 5;
-      const outerRadius = this.size;
-      const innerRadius = this.size / 1.9;
-      const step = (Math.PI * 2) / spikes;
-
-      const startingAngle = -Math.PI / 2;
-
-      ctx.moveTo(
-        this.x + Math.cos(startingAngle) * outerRadius,
-        this.y + Math.sin(startingAngle) * outerRadius
-      );
-
-      for (let i = 0; i < spikes * 2; i++) {
-        let angle = startingAngle + (step * i) / 2;
-        let radius = i % 2 === 0 ? outerRadius : innerRadius;
-
-        ctx.lineTo(
-          this.x + Math.cos(angle) * radius,
-          this.y + Math.sin(angle) * radius
-        );
-      }
-
-      ctx.closePath();
-      ctx.fill();
-      ctx.stroke();
+    async draw() {
+      const path = new Path2D(customSvgPath);
+      ctx.save();
+      ctx.translate(this.x, this.y);
+      ctx.scale(this.size / 4.4, this.size / 4.4);
+      ctx.lineWidth = 1;
+      ctx.strokeStyle = theme.colors.blue[4];
+      ctx.stroke(path);
+      ctx.restore();
     }
   }
 
@@ -80,13 +62,15 @@ function initBackgroundAnimation() {
     canvas.height = window.innerHeight;
   }
 
-  function animateParticles() {
+  async function animateParticles() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    particles.forEach((particle) => {
-      particle.update();
-      particle.draw();
-    });
+    await Promise.all(
+      particles.map((particle) => {
+        particle.update();
+        return particle.draw();
+      })
+    );
 
     requestAnimationFrame(animateParticles);
   }
@@ -95,9 +79,9 @@ function initBackgroundAnimation() {
     const posX = canvas.width / 2;
     const posY = canvas.height / 2;
 
-    const size = 22;
-    const speedX = (Math.random() - 0.5) * 4;
-    const speedY = (Math.random() - 0.5) * 4;
+    const size = 27;
+    const speedX = (Math.random() - 0.5) * 4.3;
+    const speedY = (Math.random() - 0.6) * 4.3;
 
     particles.push(new Particle(posX, posY, size, speedX, speedY));
   }
@@ -106,7 +90,7 @@ function initBackgroundAnimation() {
   animateParticles();
   window.addEventListener('resize', resizeCanvas);
 
-  for (let i = 0; i < 55; i++) {
+  for (let i = 0; i < 30; i++) {
     createParticle();
   }
 }
