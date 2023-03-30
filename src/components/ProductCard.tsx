1222371b91fc19c58f4aa50beee2672443b7b4dc
Gabriel Lugo Méndez <gabriel.lugo.mendez@gmail.com>
2023-03-30 09:13:43 +0200

    removed unused code

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -21,14 +21,13 @@ export interface Props {
 }
 
 function ProductCard({ product, sortDirection, sortedProducts }: Props) {
-  const { getProductQuantity, increaseCartQuantity } = useShoppingCart();
-  const quantity = getProductQuantity(product.id);
+  const { increaseCartQuantity } = useShoppingCart();
   const link = '/product/' + product.id;
 
-  const price =
-    sortDirection === 'lowest'
-      ? sortedProducts[0].price
-      : sortedProducts[sortedProducts.length - 1].price;
+  // const price =
+  //   sortDirection === 'lowest'
+  //     ? sortedProducts[0].price
+  //     : sortedProducts[sortedProducts.length - 1].price;
 
   return (
     <>
